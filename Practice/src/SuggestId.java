9f22c40aae5a0a5a98904f6dafd31cb6bd2db2bb
norma <norma@DESKTOP-O7HNOOT>
2021-11-24 17:27:09 +0900

    commit

--- Practice/src/SuggestId.java
+++ Practice/src/SuggestId.java
@@ -2,23 +2,48 @@
 public class SuggestId {
 
 	public static void main(String[] args) {
-		System.out.println(solution("Hello World!_HA."));
+		System.out.println(solution("Hello World!_H...ABCD."));
 
 	}
 	
 	public static String solution(String new_id) {
 		char[] id = new_id.toCharArray();
 		int len = id.length;
+		
 		for(int i=0; i<len; i++) {
 			char c = id[i];
-			if('a'<=c && c<='z' && c=='-' && c=='_' && c=='.') 
+			if(('a'<=c && c<='z') || c=='-' || c=='_' || c=='.') 
 				continue;
 			
 			if('A'<=c && c<='Z') {
 				id[i] = (char)(c+32);
-				System.out.println("YES! " + c);
 			}else {
 				remove(id, i, len);
+				len--;
+				i--;
+			}
+		}
+		
+		for(int i=0; i<len; i++) {
+			if(i==0 || i==(len-1)) {
+				if(id[i] == '.') {
+					remove(id, i, len);
+					i--;
+					len--;
+				}
+				continue;
+			}
+			if(id[i-1]=='.' && id[i]=='.') {
+				remove(id, i, len);
+				i--;
+				len--;
+			}
+		}
+		
+		if(len>15) {
+			for(int i=15; i<len; i++) {
+				remove(id, i, len);
+				i--;
 				len--;
 			}
 		}
@@ -30,8 +55,8 @@ public class SuggestId {
 	public static void remove(char[] id, int index, int len) {
 		char c;
 		for(int i=index; i<(id.length-1); i++) {
-			c = id[index+1];
-			id[index] = c;
+			c = id[i+1];
+			id[i] = c;
 		}
 	}
 
